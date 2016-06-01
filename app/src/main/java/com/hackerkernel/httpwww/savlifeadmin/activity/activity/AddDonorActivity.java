66f5e662ecd31a4312445bc4ea3e620e107cca91
murtazaagaz <murtaza.agaz100@gmail.com>
2016-06-01 15:54:18 +0530

    compleed UI1

---
+++ app/src/main/java/com/hackerkernel/httpwww/savlifeadmin/activity/activity/AddDonorActivity.java
@@ -0,0 +1,15 @@
+package com.hackerkernel.httpwww.savlifeadmin.activity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hackerkernel.httpwww.savlifeadmin.R;
+
+public class AddDonorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_donor);
+    }
+}
---
+++ app/src/main/java/com/hackerkernel/httpwww/savlifeadmin/activity/activity/HomeActivuty.java
@@ -0,0 +1,17 @@
+package com.hackerkernel.httpwww.savlifeadmin.activity.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hackerkernel.httpwww.savlifeadmin.R;
+
+public class HomeActivuty extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_activuty);
+    }
+
+
+}
