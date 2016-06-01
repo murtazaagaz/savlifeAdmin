399c2e84423970a6a0925cdda10771238eae2dbe
murtazaagaz <murtaza.agaz100@gmail.com>
2016-06-01 15:57:13 +0530

    compleed UI1

--- app/src/main/java/com/hackerkernel/httpwww/savlifeadmin/activity/activity/AddAdminActivity.java
+++ app/src/main/java/com/hackerkernel/httpwww/savlifeadmin/activity/activity/AddAdminActivity.java
@@ -2,14 +2,22 @@ package com.hackerkernel.httpwww.savlifeadmin.activity.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.hackerkernel.httpwww.savlifeadmin.R;
 
+import butterknife.Bind;
+
 public class AddAdminActivity extends AppCompatActivity {
 
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_admin);
+        setSupportActionBar(toolbar);
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setTitle("Add Admin");
     }
 }
